21a30ff326c6f19153c45e420c1551713e7486d7
Coline Velard <coline.velard@betterway.fr>
2025-05-23 17:01:56 +0200

    event sourcing part 1

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,7 +1,37 @@
-import {main} from "./main";
+import {AddTodo, apply, decide, RemoveTodo, TodoAdded, TodoRemoved} from "./main";
 
-describe('main', () => {
-    it('should work', () => {
-        expect(main()).toBe(0)
+describe('Todo List', () => {
+
+    describe('Decide', () => {
+        it('should add a todo', () => {
+
+            const addTodo: AddTodo = { name: 'AddTodo', id: 1, title: 'Buy groceries' }
+            const result = decide([], addTodo)
+
+            expect(result).toEqual({ name: 'TodoAdded', id: 1, title: 'Buy groceries' })
+        })
+
+        it('should remove a todo', () => {
+            const removeTodo: RemoveTodo = { name: 'RemoveTodo', id: 1 }
+            const result = decide([{ id: 1, title: 'Buy groceries' }], removeTodo)
+
+            expect(result).toEqual({ name: 'TodoRemoved', id: 1 })
+        })
+    })
+
+    describe('Apply', () => {
+        it('apply a todo added event', () => {
+            const addTodo: TodoAdded = { name: 'TodoAdded', id: 1, title: 'Buy groceries' }
+            const result = apply([], addTodo)
+
+            expect(result).toEqual([{ id: 1, title: 'Buy groceries' }])
+        })
+
+        it('apply a todo removed event', () => {
+            const removeTodo: TodoRemoved = { name: 'TodoRemoved', id: 1 }
+            const result = apply([{ id: 1, title: 'Buy groceries' }], removeTodo)
+
+            expect(result).toEqual([])
+        })
     })
 })
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,52 @@
+// Todo List
+
+// Events
+export type TodoAdded = {name: 'TodoAdded', id: number, title: string }
+export type TodoRemoved = {name: 'TodoRemoved', id: number }
+export type TodoEvent = TodoAdded | TodoRemoved
+
+// Commands
+export type AddTodo = { name: 'AddTodo', id: number, title: string }
+export type RemoveTodo = { name: 'RemoveTodo', id: number }
+export type Command = AddTodo | RemoveTodo
+
+// State
+export type Todo = { id: number, title: string }
+export type TodoList = Todo[]
+export type State = TodoList
+
+// Initial State
+// const initialState: State = []
+
+// decide
+type Decide = (state: State, command: Command) => TodoEvent
+
+// apply
+// type Apply = (state: State, event: TodoEvent) => State
+
+export const decide: Decide = (_state: State, command: Command): TodoEvent => {
+    if (command.name === 'AddTodo') {
+        return { name: 'TodoAdded', id: command.id, title: command.title }
+    }
+    if (command.name === 'RemoveTodo') {
+        return { name: 'TodoRemoved', id: command.id }
+    }
+    throw new Error('Unknown command')
+}
+
+export const apply = (state: State, event: TodoEvent): State => {
+    if (event.name === 'TodoAdded') {
+        return [...state, { id: event.id, title: event.title }]
+    }
+    if (event.name === 'TodoRemoved') {
+        return state.filter(todo => todo.id !== event.id)
+    }
+    else {
+        throw new Error('Unknown event')
+    }
+}
+
+
 export function main() {
     return 0
 }
